b8948a293306cf9a409648cbef61d4a714d8fffb
Bonom0 <caioazbonomo@gmail.com>
2025-09-05 14:45:01 -0300

    Criado widget para TextFormField padrão do aplicativo

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vakinha_burger/app/core/ui/widgets/vakinha_textformfield.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
@@ -7,7 +8,15 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Splash')),
-      body: Container(),
+      body: Column(
+        children: [
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: VakinhaTextformfield(label: 'Text Form'),
+          ),
+        ],
+      ),
     );
   }
 }
