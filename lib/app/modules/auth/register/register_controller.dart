c342acf1f069f6bc7479ca61859613740e9643c9
Bonom0 <caioazbonomo@gmail.com>
2025-09-08 11:29:04 -0300

    Implementado message mixin

--- lib/app/modules/auth/register/register_controller.dart
+++ lib/app/modules/auth/register/register_controller.dart
@@ -1,11 +1,14 @@
 import 'package:get/get.dart';
 import 'package:vakinha_burger/app/core/mixins/loader_mixin.dart';
+import 'package:vakinha_burger/app/core/mixins/messages_mixin.dart';
 import 'package:vakinha_burger/app/repositories/auth/auth_repository.dart';
 
-class RegisterController extends GetxController with LoaderMixin {
+class RegisterController extends GetxController
+    with LoaderMixin, MessagesMixin {
   final AuthRepository _authRepository;
 
   final loading = false.obs;
+  final message = Rxn<MessageModel>();
 
   RegisterController({required AuthRepository authRepository})
     : _authRepository = authRepository;
@@ -13,11 +16,17 @@ class RegisterController extends GetxController with LoaderMixin {
   @override
   void onInit() {
     loaderListener(loading);
+    messageListener(message);
     super.onInit();
   }
 
   void qualquer() {
-    loading.toggle();
-    Future.delayed(Duration(seconds: 2), () => loading.toggle());
+    message.value = MessageModel(
+      title: 'Teste',
+      message: 'Mensagem teste',
+      type: MessageType.info,
+    );
+    // loading.toggle();
+    // Future.delayed(Duration(seconds: 2), () => loading.toggle());
   }
 }
